9c6ec4f8e4c52251798a1acdfe243862006f674f
andrew <vip.paevskiy@gmail.com>
2022-03-24 00:26:55 +0300

    delete some trash (unused lines) from files

--- src/main/java/ru/learnUp/lesson20/springboottest/SpringBootTestApplication.java
+++ src/main/java/ru/learnUp/lesson20/springboottest/SpringBootTestApplication.java
@@ -3,15 +3,6 @@ package ru.learnUp.lesson20.springboottest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-
-import java.util.Locale;
-import java.util.Random;
-
-import static ru.learnUp.lesson20.springboottest.MyGame.AttemptCounter;
-import static ru.learnUp.lesson20.springboottest.MyGame.locale;
 
 @SpringBootApplication
 public class SpringBootTestApplication {
